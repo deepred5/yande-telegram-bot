b6431c6391204c1c5ca80509f6c10c2c92d191e8
deepred5 <948931744@qq.com>
2019-03-30 19:44:48 +0800

    modify cron task time

--- cron/index.js
+++ cron/index.js
@@ -2,12 +2,12 @@ const cron = require('node-cron');
 const http = require('../service/http');
 const config = require('../config');
 
-// 每晚21:00发送今日popular
+// 每晚20:00发送今日popular
 const initTask = (bot) => {
   console.log('cron task start');
-  cron.schedule('0 21 * * *', async () => {
+  cron.schedule('0 20 * * *', async () => {
     const { data = [] } = await http.getYandePopularPic({ period: '1d' });
-    bot.sendMessage(config.dailyYandeChannelId, '每日popular');
+    bot.sendMessage(config.dailyYandeChannelId, '每日POPULAR');
     data.forEach(img => {
       bot.sendPhoto(config.dailyYandeChannelId, img.file_url);
     });
