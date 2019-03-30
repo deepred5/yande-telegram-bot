a920e3bbcb80fd35ff59087b5973d3cfc1726a5c
deepred5 <948931744@qq.com>
2019-03-30 21:08:19 +0800

    add cron task

--- cron/index.js
+++ cron/index.js
@@ -2,16 +2,29 @@ const cron = require('node-cron');
 const http = require('../service/http');
 const config = require('../config');
 
-// 每晚20:00发送今日popular
+const getPeriod = (bot, period) => async () => {
+  const { data = [] } = await http.getYandePopularPic({ period });
+  const titleMap = {
+    '1d': '每日',
+    '1w': '每周',
+    '1m': '每月'
+  }
+  bot.sendMessage(config.dailyYandeChannelId, `${titleMap[period]}POPULAR`);
+  data.forEach(img => {
+    bot.sendPhoto(config.dailyYandeChannelId, img.file_url);
+  });
+};
+
 const initTask = (bot) => {
   console.log('cron task start');
-  cron.schedule('0 20 * * *', async () => {
-    const { data = [] } = await http.getYandePopularPic({ period: '1d' });
-    bot.sendMessage(config.dailyYandeChannelId, '每日POPULAR');
-    data.forEach(img => {
-      bot.sendPhoto(config.dailyYandeChannelId, img.file_url);
-    });
-  });
+  // 每天20:00发送今日popular
+  cron.schedule('0 20 * * *', getPeriod(bot, '1d'));
+
+  // 每周21:00发送本周popular
+  cron.schedule('0 21 * * 0', getPeriod(bot, '1w'));
+
+  // 每月最后一天22:00发送本月popular
+  cron.schedule('0 22 L * *', getPeriod(bot, '1m'));
 }
 
 module.exports = {
